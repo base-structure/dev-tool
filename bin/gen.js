cdb088022ca2c30ef09163ab8300e8ddd221b71e
nick.na <nickna@augmentum.com.cn>
2019-08-21 14:22:05 +0800

    feat(html): base html template

--- bin/gen.js
+++ bin/gen.js
@@ -3,30 +3,19 @@
 var path = require('path');
 var program = require('commander');
 var colors = require( "colors");
-var layout = require( "../lib/layout");
+var html = require( "../lib/html");
 
 var targetPath = process.cwd();
 
 program
   .version('1.0.0')
-  .option('--type [value]', 'Generate layout type')
-  .option('-f, --filename [value]', 'Target filename')
-
-program
-    .command('layout <filename>')
-    .option('--type [value]', 'Generate layout type')
-    .description('Generate HTML amd css')
-    .action(function(filename, options){
-        layout.gen(filename);
-    });
 
 program
     .command('html <name>')
-    .option('-t, --target [value]', 'Target file path')
-    .option('-f, --filename [value]', 'Target filename')
-    .description('Generate a base vue file')
+    .description('Generate HTML')
     .action(function(name, options){
-        console.log('Create %s', name, options.filename);
+        html.gen(name);
+        console.log('Create %s', name, options);
     });
 
 program.parse(process.argv);
