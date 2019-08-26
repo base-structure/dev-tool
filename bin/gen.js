5a8c3086815f1f7fca693fa5c6fd43a7f0a5cace
nick.na <nickna@augmentum.com.cn>
2019-08-26 16:09:38 +0800

    代码重构

--- bin/gen.js
+++ bin/gen.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var program = require('commander');
-var colors = require( 'colors');
-var genrate = require( '../lib/genrate');
-var config = require('../lib/config.js');
+var path = require('path')
+var program = require('commander')
+var colors = require( 'colors')
+var genrate = require( '../lib/genrate')
+var config = require('../lib/config.js')
 
-var targetPath = process.cwd();
+var targetPath = process.cwd()
 
 program
   .version('1.0.0')
 
-config.commands.forEach(item => {
-    let types = Object.keys(config.templateMap[item]).join(', ');
+Object.keys(config.commands).forEach(item => {
+    let types = Object.keys(config.commands[item].template).join(', ')
 
     program
         .command(item)
@@ -20,8 +20,8 @@ config.commands.forEach(item => {
         .option('-f, --filename <filename>', '文件名')
         .option('-t, --type <type>', '类型    [' + types + ']')
         .action(function(options){
-            genrate(item, options);
-        });
+            genrate(item, options)
+        })
 })
 
-program.parse(process.argv);
+program.parse(process.argv)
